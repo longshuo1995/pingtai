02f87622f0f6789e28113ccfc1c22a6434a7a3ba
longshuo <longshuo@zuoyebang.com>
2018-12-20 00:13:29 +0800

    t

--- app/user/asset_account.py
+++ app/user/asset_account.py
@@ -3,34 +3,34 @@ from flask import render_template, request
 import time
 
 from db import db_mongo
-#
-#
-# def get_init_data():
-#     res = db_mongo.account.qq_account.find()
-#     data = {
-#         'msg': 'hi',
-#         'accounts': []
-#     }
-#     for i in res:
-#         data['accounts'].append(i)
-#     return data
-#
-#
-# @bl_user.route('/save_account')
-# def save_account():
-#     username = request.args.get('username')
-#     password = request.args.get('password')
-#     data = get_init_data()
-#     if not username or not password:
-#         data['msg'] = '请输入账号密码'
-#     elif db_mongo.account.qq_account.find({'account': username.strip()}):
-#         data['msg'] = '账号已存在~'
-#     else:
-#         db_mongo.account.insert_one({
-#             'account': username,
-#             'password': password,
-#             'timestamp': int(time.time()),
-#         })
-#         data['msg'] = '提交成功'
-#
-#     return render_template('user/receive.html', data=data)
+
+
+def get_init_data():
+    res = db_mongo.account.qq_account.find()
+    data = {
+        'msg': 'hi',
+        'accounts': []
+    }
+    for i in res:
+        data['accounts'].append(i)
+    return data
+
+
+@bl_user.route('/save_account')
+def save_account():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    data = get_init_data()
+    if not username or not password:
+        data['msg'] = '请输入账号密码'
+    elif db_mongo.account.qq_account.find({'account': username.strip()}):
+        data['msg'] = '账号已存在~'
+    else:
+        db_mongo.account.insert_one({
+            'account': username,
+            'password': password,
+            'timestamp': int(time.time()),
+        })
+        data['msg'] = '提交成功'
+
+    return render_template('user/receive.html', data=data)
--- app/web_manager.py
+++ app/web_manager.py
@@ -3,41 +3,6 @@ from app import create_app
 from conf import setting
 
 app = create_app()
-from flask import render_template, request
-import time
-
-from db import db_mongo
-
-
-def get_init_data():
-    res = db_mongo.account.qq_account.find()
-    data = {
-        'msg': 'hi',
-        'accounts': []
-    }
-    for i in res:
-        data['accounts'].append(i)
-    return data
-
-
-@app.route('/save_account')
-def save_account():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    data = get_init_data()
-    if not username or not password:
-        data['msg'] = '请输入账号密码'
-    elif db_mongo.account.qq_account.find({'account': username.strip()}):
-        data['msg'] = '账号已存在~'
-    else:
-        db_mongo.account.insert_one({
-            'account': username,
-            'password': password,
-            'timestamp': int(time.time()),
-        })
-        data['msg'] = '提交成功'
-
-    return render_template('user/receive.html', data=data)
 
 
 if __name__ == '__main__':
